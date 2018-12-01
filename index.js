24f7dda94ec91fe8599e0da8dbda23dbb9f45a75
Trubetskoy <42418728+Trubetskoy@users.noreply.github.com>
2018-12-01 18:38:07 +0200

    enter to add new tasks, edit, fixed algorithm in loops

--- index.js
+++ index.js
@@ -5,12 +5,13 @@ let id = getStore('id');
 const element = document.getElementById("toDoRoot");
 const newTaskButton = document.getElementById('newTask');
 const input = document.getElementById('newTaskText');
- 
+const enter = document.getElementById('enter');
+
 
 let startRender = function () {
-    toDoList.forEach((item) => {
-        render(item)
-    })
+    for (let i=0; toDoList.length; i++){
+        render(toDoList[i])
+    }
 };
 
 function render(task) {
@@ -18,19 +19,24 @@ function render(task) {
     let node2 = document.createTextNode('edit');
     let node3 = document.createTextNode('delete');
     let editInput = document.createElement('input');
+    let toDoText = document.createElement('p');
+    let toDoTask = document.createTextNode (task.value);
     let editButton = document.createElement('button');
     let deleteButton = document.createElement('button');
     let checkBox = document.createElement('input');
 
     checkBox.type='checkbox';
-    checkBox.className = 'checkbox'
-    editInput.className = 'taskInput'
+    checkBox.className = 'checkbox';
+    editInput.className = 'taskInput';
     paragraph.className = 'paragraph';
     editButton.className = 'editButton';
     deleteButton.className = 'deleteButton';
+    toDoText.className = 'toDoText';
     editInput.value = task.value;
+    paragraph.appendChild(toDoText);
     paragraph.appendChild(editInput);
     paragraph.appendChild(checkBox);
+    toDoText.appendChild(toDoTask);
     paragraph.name = task.id;
     paragraph.className = task.status;
     editButton.appendChild(node2);
@@ -42,7 +48,7 @@ function render(task) {
     editButton.addEventListener('click', (event)=>{editFunc(event, task.id)});
     deleteButton.addEventListener('click', (event)=>{deleteFunc(event, task.id)});
     checkBox.addEventListener('click', (event)=> {checkFunc(event, task.id)});
-
+    
 };
 
 function putStore(newObject, id) {
@@ -68,7 +74,7 @@ if (action === 'toDoList'){
     }
 }
 
-newTaskButton.addEventListener('click', (event) => {
+enter.addEventListener('submit', (event) => {
     let value = input.value;
     toDoList.push({value: value, id: id, status: 'new'});
     render({value: value, id: id, status: 'new'});
@@ -78,18 +84,32 @@ newTaskButton.addEventListener('click', (event) => {
 });
 
 function editFunc(event){
-    let newValue = event.target.parentNode.firstChild.value;
-    let id = event.target.parentNode.name
-    toDoList.forEach(function (item, index){
-        if (id === item.id) {
-            item.value = newValue;
-             }
-        })
-        putStore(toDoList)
+    let newValue = event.target.parentNode.getElementsByTagName('input')[0].value;
+    let id = event.target.parentNode.name;
+    let parentNode = event.target.parentNode.getElementsByTagName('p')[0].firstChild;
+    let changeClass = event.target.parentNode.getElementsByTagName('input')[0];
+         
+    let array = toDoList
+    for (let i = 0; i < array.length; i++){ 
+        if (id === array[i].id) {
+            array[i].value = newValue;
+            }
+        
+        putStore(toDoList);
+        parentNode.textContent = newValue;
 
-} 
+        if (changeClass.className === 'taskInput') {
+            changeClass.className = 'taskInputEdit'
+        }
+        else if (changeClass.className === 'taskInputEdit') {
+                 changeClass.className = 'taskInput'
+        }
+        break
+    }
+
+};
+
 function checkFunc(event){
-    console.log(event);
     let id = event.target.parentNode.name
     let paragraphClass;
     if (event.target.checked) {
@@ -97,29 +117,41 @@ function checkFunc(event){
     }
     else {paragraphClass = 'new'}
 
-    element.childNodes.forEach(function (item, index){
-        console.log (item.name)
-     if (id === item.name) {
-        item.className = paragraphClass
-        toDoList[index].status = paragraphClass
-          }
-    })
+    let array = element.childNodes;
+    for (let i = 0; i < array.length; i++){
+    if (id === array[i].name) {
+        array[i].className = paragraphClass
+        toDoList[i].status = paragraphClass
+        break
+        }
+    }
     putStore(toDoList)
-
 }
-
 
 function deleteFunc (event){
    let id = event.target.parentNode.name
-   element.childNodes.forEach(function (item, index){
-    if (id === item.name) {
-        element.removeChild(item)
-          toDoList.splice(index, 1)
-         }
-    })
-    putStore(toDoList)
-}
-
-
-
+   let array = element.childNodes;
+   
+   for (let i = 0; i < array.length; i++){
+        if (id === array[i].name) {
+            element.removeChild(array[i])
+            toDoList.splice(i, 1)
+            break
+            }
+        }
+            putStore(toDoList)
+   }
+       
 startRender();
+
+// function deleteFunc (event){
+//     let id = event.target.parentNode.name
+//     element.childNodes.forEach(function (item, index){
+//      if (id === item.name) {
+//          element.removeChild(item)
+//            toDoList.splice(index, 1)
+//           }
+//      })
+//      putStore(toDoList)
+//  }
+ 
